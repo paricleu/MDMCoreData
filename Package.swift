fd9d8cb5e81260b3e536b24832a8d70880687902
P <p@m.com>
2026-05-28 12:21:55 +0200

    restore: back to subdirectory headers with search paths

--- Package.swift
+++ Package.swift
@@ -20,6 +20,12 @@ let package = Package(
             publicHeadersPath: ".",
             cSettings: [
                 .headerSearchPath("."),
+                .headerSearchPath("MDMCoreDataCore"),
+                .headerSearchPath("MDMFetchedResultsCollectionDataSource"),
+                .headerSearchPath("MDMFetchedResultsTableDataSource"),
+                .headerSearchPath("MDMPersistenceController"),
+                .headerSearchPath("NSManagedObject+MDMCoreDataAdditions"),
+                .headerSearchPath("Categories"),
             ]
         )
     ]
